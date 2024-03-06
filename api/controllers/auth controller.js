5f178b1b712b03a9751be6e5125a1e7e193c7292
Udayasree-Banothu444 <udayasreebanoth444@gmail.com>
2024-03-06 15:02:19 +0530

    create signin api route

--- api/controllers/auth controller.js
+++ api/controllers/auth controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from '../utils/error.js';
+import  jwt  from 'jsonwebtoken';
 
 export const signup = async (req, res, next)   => {
     // console.log(req.body); //next is added blz we are using middle ware
@@ -28,8 +29,39 @@ export const signup = async (req, res, next)   => {
     //    res.status(500).json({message: error.message}); //this will show if an user wants to signup again or same username is added again
     next(error);
     };
-
-
-    
-
 };
+
+
+export const signin =async(req,res,next) => {
+    const {email, password} = req.body;
+
+    if(!email || !password || email==='' || password === ''){
+        return next(errorHandler(400, 'All fields are required'));
+    }
+
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(404,'User not found'));
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword){
+            return next(errorHandler(404,'Invalid Password'));
+        }
+        const token = jwt.sign(
+        { id: validUser._id }, process.env.JWT_SECRET ); /*JWT_SECRET is a secret key whihc is know to the user only so that is included in .env file and here just using the reference name only */
+        
+
+        const{password: pass, ...rest}= validUser._doc;
+
+        res.status(200).cookie('access_token',token,{
+            httpOnly: true
+        }).json('rest'); /*creating a response using cookie*/
+
+        
+    }
+    catch(error){
+        next(error);
+    }
+
+}
