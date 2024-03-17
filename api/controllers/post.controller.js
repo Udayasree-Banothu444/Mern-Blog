6f1f373a5f5dd742941ff831d6acfc25472d6f8a
Udayasree-Banothu444 <udayasreebanoth444@gmail.com>
2024-03-17 20:41:53 +0530

    Create get post api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -27,3 +27,50 @@ export const create= async(req, res, next) =>{
         next(error);
     }
 }
+
+//this router can be user everywhere later
+export const getposts =async(req, res, next) =>{
+    try{
+        //first get some posts and then click on showmore to get the rest of them
+        const startIndex = parseInt(req.query.startIndex)  || 0; //it is a number like start fetching from 9number likewise
+        const limit =parseInt(req.query.limit) ||9; //first it will show 9 and then 9 
+        const sortDirection = req.query.order === 'asc' ? 1: -1;
+        const posts= await Post.find({
+            ...(req.query.userId && {userId: req.query.userId}), 
+            ...(req.query.category && {category: req.query.userId}),
+            ...(req.query.slug && {category: req.query.slug}),
+            ...(req.query.postId && {_id: req.query.postId}),
+            ...(req.query.searchTerm && {
+                $or: [
+                    {title: {$regex: req.query.searchTerm, $options:'i'}},
+                    {content: {$regex: req.query.searchTerm, $options:'i'}},
+                ],
+            }),
+        }).sort({updatedAt : sortDirection}).skip(startIndex).limit(limit);
+
+        
+        const totalPosts =await Post.countDocuments(); // we want total no.of posts also
+        const now =new Date();// we also want to know in last month how many posts are created
+        const oneMonthAgo = new Date(
+            now.getFullYear(),
+            now.getMonth()-1,
+            now.getDate()
+        );// gets date from today to the last month
+        const lastMonthPosts = await Post.countDocuments({
+            createdAt: {$gte: oneMonthAgo},
+        });
+        
+        res.status(200).json({
+            posts,
+            totalPosts,
+            lastMonthPosts,
+        });
+
+
+
+    }
+    catch(error){
+        next(error);
+    }
+
+}
--- api/route/post.route.js
+++ api/route/post.route.js
@@ -2,9 +2,11 @@
 import express  from "express";
 import { verifyToken } from "../utils/verifyUser.js";
 import { create } from "../controllers/post.controller.js";
+import { getposts } from "../controllers/post.controller.js";
 
 const router= express.Router();
 
 router.post('/create', verifyToken, create);
+router.get('/getposts',getposts);//we dont want token blz anyone can view the posts
 
 export default router;
