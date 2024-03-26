f8c8c6b8a5112ef8d02cfc405134be8808e1d263
Udayasree-Banothu444 <udayasreebanoth444@gmail.com>
2024-03-26 23:02:18 +0530

    Complete About and Project Pages

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font-semibold text-center my-7'>About Udaya's Blog</h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>Welcome to Udaya's blog!! This blog was created by Udayasree Banothu as a personal project to share her thoughts and ideas with the world</p>
+            <p>
+              Udaya's blog is a blog that I have created to share my thoughts and ideas with the world.I am a Btech undergraduate student, currently in 3rd year and I love to write about my experiences and things that I have learned..I hope this blog can be useful to many people..
+            </p>
+            <p>We encourage you to leave comments on our posts and engage with other readers.You can like other people's comments and reply to them as well.We beleve that a community of learners can help each other grow and improve</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
